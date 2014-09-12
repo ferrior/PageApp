d4f7a875ed86a21da7e94667d28004df8972ef3b
Bin Liu <bliu@adobe.com>
2014-09-12 15:06:16 +0800

    Add new view controller

---
+++ APPChildViewController.h
@@ -0,0 +1,13 @@
+//
+//  APPChildViewController.h
+//  PageApp
+//
+//  Created by Bin Liu on 9/12/14.
+//  Copyright (c) 2014 Bin Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APPChildViewController : UIViewController
+
+@end
---
+++ APPChildViewController.m
@@ -0,0 +1,38 @@
+//
+//  APPChildViewController.m
+//  PageApp
+//
+//  Created by Bin Liu on 9/12/14.
+//  Copyright (c) 2014 Bin Liu. All rights reserved.
+//
+
+#import "APPChildViewController.h"
+
+@interface APPChildViewController ()
+
+@end
+
+@implementation APPChildViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
